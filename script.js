8b57dc06db83108bbbfa22137b8bf99c65037c30
Varga Dávid <nixtrox.vidi@gmail.com>
2021-04-30 21:01:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let i = -1
+
+function addElement() {
+
+
+    while(i < 20){
+    i = i+1;
+    var table = document.getElementById("Table");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = i;
+    
+
+
+    let yesss = Factorialize(i)
+    console.log("The factorial of " + i + " is " + yesss);
+    
+    cell2.innerHTML = yesss;
+  }
+
+}
+
+
+function Factorialize(x){
+    if( x == 0 || x == 1){
+        return 1;
+    }
+    else{
+        return x * Factorialize(x - 1);
+    }
+}
+
+  addElement();
+  
