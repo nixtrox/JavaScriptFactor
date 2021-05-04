e2d21ba5c38190e50a4e168daa2dd7631b27078e
Varga Dávid <nixtrox.vidi@gmail.com>
2021-05-04 12:53:59 +0200

    adding the "!"

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function addElement() {
     var row = table.insertRow(-1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    cell1.innerHTML = i;
+    cell1.innerHTML = i+"!";
     
 
 
